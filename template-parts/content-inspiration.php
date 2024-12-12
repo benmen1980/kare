12a2780643399ed5d31a620e4ffc9e9f5ceb3341
margalit-t <132431154+margalit-t@users.noreply.github.com>
2024-12-12 17:07:20 +0200

    add post inspiration

--- template-parts/content-inspiration.php
+++ template-parts/content-inspiration.php
@@ -3,11 +3,12 @@
     $array_pdts = get_field('select_pdts',  get_the_ID() );
     // var_dump($array_pdts);
 
-    $per_page = get_option('posts_per_page');
-    $current_page = max(1, get_query_var('paged', 1));
-    if ( ! empty( $array_pdts ) && is_array( $array_pdts ) )
+    if ( ! empty( $array_pdts ) && is_array( $array_pdts ) ) :
+        $per_page = get_option('posts_per_page');
+        $current_page = max(1, get_query_var('paged', 1));
         $total_products = count($array_pdts);
-    $total_pages = ceil($total_products / $per_page);
+        $total_pages = ceil($total_products / $per_page);
+    endif;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -80,86 +81,88 @@
         <?php endif; ?>
 
     </section>
-    
-    <section class="container-archive-product show-archive-product container">
 
-        <div class="product_sorting_wrapper">
-            <div class="hidden"></div>
-            <div class="product-count">
-                <span class="number-pdts">
-                    <?php echo sprintf(
-                                esc_html__('%s Products', 'kare'),
-                                $total_products
-                            ); 
-                    ?>
-                </span>
-                <span> | </span>
-                <span class="number-page">
-                    <?php echo sprintf(
-                                esc_html__('%s of %s', 'kare'),
-                                $current_page,
-                                $total_pages
-                            ); 
-                    ?>
-                </span>
-            </div>
-            <div class="sorting-options">
-            </div>
-        </div>
+    <?php if ( ! empty( $array_pdts ) && is_array( $array_pdts ) ) : ?>
+        <section class="container-archive-product show-archive-product container">
 
-        <div class="archive-product">
-            <?php
-            if ( ! empty( $array_pdts ) && is_array( $array_pdts ) ) :
-                foreach( $array_pdts as $product) :
-                    setup_postdata( $product );
-                    // Pass a custom variable to the template
-                    set_query_var('is_archive_product_inspiration', false);
-                    get_template_part('page-templates/box-product'); 
-                    wp_reset_postdata(); 
-                endforeach;
-            else :
-                echo '<p>No products found.</p>';
-            endif;
-            ?>
-        </div>
-
-        <!-- load more pages of same category -->
-        <?php if ($total_pages > 1) : ?>
-            <div class="load_more_pdts_wrapper">
-
-                <!-- Previous Button -->
-                <?php if ($current_page > 1): ?>
-                    <a href="<?php echo get_pagenum_link($current_page - 1); ?>" class="pagination-prev btn_white_hover">
-                        <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-right-md.svg'); ?>
-                        <span class="prev"><?php echo esc_html_e('Previous', 'kare'); ?></span>
-                    </a>
-                <?php endif; ?>
-                    
-                <!-- Dropdown Pagination -->
-                <div class="pagination-dropdown">
-                    <button class="pagination-current">
-                        <span><?php echo $current_page . ' of ' . $total_pages; ?></span>
-                        <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-down.svg'); ?>
-                    </button>
-                    <ul class="pagination-options">
-                        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
-                            <li><a href="<?php echo get_pagenum_link($i); ?>" <?php echo ($i === $current_page) ? 'class="active"' : ''; ?>>
-                                <?php echo $i; ?>
-                            </a></li>
-                        <?php endfor; ?>
-                    </ul>
+            <div class="product_sorting_wrapper">
+                <div class="hidden"></div>
+                <div class="product-count">
+                    <span class="number-pdts">
+                        <?php echo sprintf(
+                                    esc_html__('%s Products', 'kare'),
+                                    $total_products
+                                ); 
+                        ?>
+                    </span>
+                    <span> | </span>
+                    <span class="number-page">
+                        <?php echo sprintf(
+                                    esc_html__('%s of %s', 'kare'),
+                                    $current_page,
+                                    $total_pages
+                                ); 
+                        ?>
+                    </span>
                 </div>
-                    
-                <!-- Next Button -->
-                <?php if ($current_page < $total_pages): ?>
-                    <a href="<?php echo get_pagenum_link($current_page + 1); ?>" class="pagination-next btn_white_hover">
-                        <span class="next"><?php echo esc_html_e('Next', 'kare'); ?></span>
-                        <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-right-md.svg'); ?>
-                    </a>
-                <?php endif; ?>
-                    
-            </div>	
-        <?php endif; ?>
+                <div class="sorting-options">
+                </div>
+            </div>
 
-    </section>
+            <div class="archive-product">
+                <?php
+                if ( ! empty( $array_pdts ) && is_array( $array_pdts ) ) :
+                    foreach( $array_pdts as $product) :
+                        setup_postdata( $product );
+                        // Pass a custom variable to the template
+                        set_query_var('is_archive_product_inspiration', false);
+                        get_template_part('page-templates/box-product'); 
+                        wp_reset_postdata(); 
+                    endforeach;
+                else :
+                    echo '<p>No products found.</p>';
+                endif;
+                ?>
+            </div>
+
+            <!-- load more pages of same category -->
+            <?php if ($total_pages > 1) : ?>
+                <div class="load_more_pdts_wrapper">
+
+                    <!-- Previous Button -->
+                    <?php if ($current_page > 1): ?>
+                        <a href="<?php echo get_pagenum_link($current_page - 1); ?>" class="pagination-prev btn_white_hover">
+                            <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-right-md.svg'); ?>
+                            <span class="prev"><?php echo esc_html_e('Previous', 'kare'); ?></span>
+                        </a>
+                    <?php endif; ?>
+                        
+                    <!-- Dropdown Pagination -->
+                    <div class="pagination-dropdown">
+                        <button class="pagination-current">
+                            <span><?php echo $current_page . ' of ' . $total_pages; ?></span>
+                            <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-down.svg'); ?>
+                        </button>
+                        <ul class="pagination-options">
+                            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                <li><a href="<?php echo get_pagenum_link($i); ?>" <?php echo ($i === $current_page) ? 'class="active"' : ''; ?>>
+                                    <?php echo $i; ?>
+                                </a></li>
+                            <?php endfor; ?>
+                        </ul>
+                    </div>
+                        
+                    <!-- Next Button -->
+                    <?php if ($current_page < $total_pages): ?>
+                        <a href="<?php echo get_pagenum_link($current_page + 1); ?>" class="pagination-next btn_white_hover">
+                            <span class="next"><?php echo esc_html_e('Next', 'kare'); ?></span>
+                            <?php echo file_get_contents(get_template_directory_uri() . '/dist/images/svg/arrow-right-md.svg'); ?>
+                        </a>
+                    <?php endif; ?>
+                        
+                </div>	
+            <?php endif; ?>
+
+        </section>
+    <?php endif; ?>
 </article>
